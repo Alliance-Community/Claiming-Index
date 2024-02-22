d5f7b9a48cd0df8584df26cbf23c5a362abf31cf
CAS_ual_TY <41904979+CAS-ual-TY@users.noreply.github.com>
2024-02-22 11:57:52 +0100

    Initial commit

---
+++ gen_asset_index.py
@@ -0,0 +1,163 @@
+from icon_lister import *
+import time
+
+ignore_list = [
+    "us_air_c47_para",
+    "civilianCar2",
+    "arg_jet_a1h_objective"
+]
+
+def makeHead(title):
+    return [
+        "<!DOCTYPE html>",
+        "<html>",
+        "<head>",
+        "<title>{}</title>".format(title),
+        "<style>",
+        "html {"
+        "    font: 12px/1.5 Arial, sans-serif;",
+        "    background: lightgray;",
+        "}",
+        "img {",
+        "    image-rendering: -moz-crisp-edges;",
+        "    image-rendering: -webkit-optimize-contrast;",
+        "    image-rendering: crisp-edges;",
+        "    -ms-interpolation-mode: nearest-neighbor;",
+        "    height: 64px;",
+        "}",
+        "th, td {",
+        "    border: 1px solid black;",
+        "    vertical-align: top;",
+        "    padding: 8px;",
+        "}",
+        "</style>",
+        "</head>",
+        "<body>",
+        "",
+        "<h1>{}</h1>".format(title),
+        "<h2>Automatically generated at {} using a script by CAS_ual_TY</h2>".format(time.asctime()),
+        "",
+    ]
+
+tail = [
+    "",
+    "</body>",
+    "</html>",
+]
+
+facTr = {
+    "civ": "Civilian",
+    "gb82": "GB",
+    "mil": "Militia"
+}
+
+def vehToFactionName(veh):
+    vehOrig = veh
+    fac = None
+    
+    for i in range(2, 5):
+        if veh[i] == "_":
+            fac = veh[:i]
+            break
+    
+    if veh in names_map:
+        veh = names_map[veh]
+        if veh[0] == "\"":
+            veh = veh[1:]
+        if veh[-1] == "\"":
+            veh = veh[:-1]
+    else:
+        print("No HUD name found for: {}".format(vehOrig))
+    
+    if fac == None:
+        print("Unknown faction: {}".format(vehOrig))
+        return veh
+    
+    if fac in facTr:
+        fac = facTr[fac]
+    else:
+        fac = fac.upper()
+    
+    return (fac, veh)
+
+if __name__ == "__main__":
+    # ########################################################################################
+    # Icon -> Asset Index
+    # ########################################################################################
+
+    file = os.path.join(os.path.curdir + "/icon-to-asset_index.html")
+    print(file)
+
+    lines = []
+    lines.extend(makeHead("PR Icon-to-Assets Index"))
+    lines.extend([
+        "<h3>Sorted alphabetically by asset name (ignoring faction identifier in front).</h3>",
+        "<table>"
+    ])
+    
+    for k, v in icons_map.items():
+        v = [a for a in v if a not in ignore_list]
+        if k[:len("mini_")] == "mini_" and len(v) > 0:
+            i = k[len("mini_"):-len(".tga")]
+            lines.append("<tr>")
+            lines.append("<td><img src=\"./original_png/{}\"></td>".format("mini_" + i + ".png"))
+            lines.append("<td>")
+            lines.append("<b>{}</b>:".format(i))
+            lines.append("<ul>")
+            for v2 in v:
+                if v2[-len(bf2):] != bf2 and v2[-len(prsp):] != prsp and v2[-len(sp):] != sp:
+                    fac, veh = vehToFactionName(v2)
+                    lines.append("<li>{} <b>{}</b> ({})</li>".format(fac, veh, v2))
+            lines.append("</ul>")
+            lines.append("</td>")
+            lines.append("</tr>")
+    lines.extend([
+        "",
+        "</table>"
+    ])
+    lines.extend(tail)
+    with open(file, "w") as f:
+        f.writelines([l + "\n" for l in lines])
+        f.close()
+
+    # ########################################################################################
+    # Asset -> Icon Index
+    # ########################################################################################
+
+    file = os.path.join(os.path.curdir + "/asset-to-icon_index.html")
+    print(file)
+    
+    lines = []
+    lines.extend(makeHead("PR Assets-to-Icon Index"))
+    lines.extend([
+        "<h3>Sorted alphabetically by asset name (ignoring faction identifier in front).</h3>",
+        "<table>"
+    ])
+    
+    assets = []
+    
+    for k, v in icons_map.items():
+        v = [a for a in v if a not in ignore_list]
+        if k[:len("mini_")] == "mini_" and len(v) > 0:
+            for v2 in v:
+                if v2[-len(bf2):] != bf2 and v2[-len(prsp):] != prsp and v2[-len(sp):] != sp:
+                    assets.append(vehToFactionName(v2) + (v2, k[len("mini_"):-len(".tga")],))
+    
+    sorted = sorted(assets, key=lambda tup: tup[1])
+
+    for fac, name, veh, icon in sorted:
+        lines.append("<tr>")
+        lines.append("<td><img src=\"./original_png/{}\"></td>".format("mini_" + icon + ".png"))
+        lines.append("<td>")
+        lines.append("{} <b>{}</b> ({})".format(fac, name, veh))
+        lines.append("</td>")
+        lines.append("</tr>")
+    lines.extend([
+        "",
+        "</table>"
+    ])
+    lines.extend(tail)
+    with open(file, "w") as f:
+        f.writelines([l + "\n" for l in lines])
+        f.close()
+
---
+++ icon_lister.py
@@ -0,0 +1,244 @@
+import os
+import sys
+
+pr_repo = sys.argv[1]
+
+minimap_icons_path = os.path.join(pr_repo, "menu/HUD/Texture/Ingame/Vehicles/Icons/Minimap")
+menuIcons_path = os.path.join(pr_repo, "menu/HUD/Texture/Ingame/Vehicles/Icons/Hud/MenuIcons")
+
+vehicles_path = "objects/vehicles/"
+vehicles_subfolders = ["Air", "civilian", "Land", "Sea"]
+
+factions = ["arg", "cf", "ch", "fr", "gb", "ger", "idf", "mec", "nl", "nva", "pl", "ru", "us", "civ", "fsa", "gb82", "mil"]
+
+prefix1 = "ObjectTemplate.vehicleHud.miniMapIcon Ingame\\Vehicles\\Icons\\Minimap\\"
+prefix2 = "ObjectTemplate.vehicleHud.typeIcon Ingame\\Vehicles\\Icons\\Hud\\MenuIcons\\"
+prefixName = "ObjectTemplate.vehicleHud.hudName "
+
+pre1 = "mini_"
+pre2 = "menuIcon_"
+
+icons_list = []
+menuIcons_list = []
+
+for f in os.listdir(minimap_icons_path):
+    if not os.path.isdir(f) and f[-len(".tga"):] == ".tga":
+        icons_list.append(f)
+
+for f in os.listdir(menuIcons_path):
+    if not os.path.isdir(f) and f[-len(".tga"):] == ".tga":
+        menuIcons_list.append(f)
+
+icons_map = {}
+names_map = {}
+
+missing_icons = {}
+missing_menuIcons = {}
+
+inconsistent_icons = []
+inconsistent_menuIcons = []
+inconsistent_names = []
+
+wrong_menuIcons = []
+
+base_vehicles = {}
+bf2_vehicles = []
+prsp_vehicles = []
+sp_vehicles = []
+
+bf2 = "_bf2"
+prsp = "_prsp"
+sp = "_sp"
+
+for icon in icons_list:
+    icons_map[icon] = []
+
+for f1 in vehicles_subfolders:
+    cur_path = os.path.join(pr_repo, vehicles_path + f1)
+    for f2 in os.listdir(cur_path):
+        if f2[0:2] in factions or f2[0:3] in factions:
+            vehicle = os.path.join(pr_repo, vehicles_path + f1 + "/" + f2)
+            if os.path.isdir(vehicle):
+                for tweak in os.listdir(vehicle):
+                    #print(tweak)
+                    #print("  {}".format(tweak[-len(".tweak"):]))
+                    if tweak[-len(".tweak"):] == ".tweak":
+                        p = os.path.join(pr_repo, vehicles_path + f1 + "/" + f2 + "/" + tweak)
+                        with open(p) as f:
+                            veh = tweak[:-len(".tweak")]
+                            icon = None
+                            menuIcon = None
+                            name = None
+                            consistentMini = True
+                            for line in f.readlines():
+                                if line.lower()[:len(prefix1)] == prefix1.lower():
+                                    icon2 = line[len(prefix1):].rstrip()
+                                    if icon == None:
+                                        icon = icon2
+                                    else:
+                                        if icon[-len(".tga"):] != ".tga":
+                                            print("!!!!!!! {} {}".format(veh, icon2))
+                                        if icon != icon2 and veh not in inconsistent_icons:
+                                            inconsistent_icons.append(veh)
+                                            consistentMini = False
+                                elif line.lower()[:len(prefix2)] == prefix2.lower():
+                                    menuIcon2 = line[len(prefix2):].rstrip()
+                                    if menuIcon == None:
+                                        menuIcon = menuIcon2
+                                    else:
+                                        if menuIcon[-len(".tga"):] != ".tga":
+                                            print("!!!!!!! {} {}".format(veh, menuIcon2))
+                                        if menuIcon != menuIcon2 and veh not in inconsistent_menuIcons:
+                                            inconsistent_menuIcons.append(veh)
+                                elif line.lower()[:len(prefixName)] == prefixName.lower():
+                                    name2 = line[len(prefixName):].rstrip()
+                                    if name == None:
+                                        name = name2
+                                    else:
+                                        if name != name2 and veh not in inconsistent_names:
+                                            inconsistent_names.append(veh)
+                            if icon == None:
+                                print("No mini_ icon found for: {}".format(veh))
+                            elif icon not in icons_list:
+                                if icon not in missing_icons:
+                                    missing_icons[icon] = []
+                                if veh not in missing_icons[icon]:
+                                    missing_icons[icon].append(veh)
+                            elif veh not in icons_map[icon]:
+                                icons_map[icon].append(veh)
+
+                            if menuIcon == None:
+                                print("No menuIcon_ icon found for: {}".format(veh))
+                            elif menuIcon not in menuIcons_list:
+                                if menuIcon not in missing_menuIcons:
+                                    missing_menuIcons[menuIcon] = []
+                                if veh not in missing_menuIcons[menuIcon]:
+                                    missing_menuIcons[menuIcon].append(veh)
+
+                            if icon == None and menuIcon == None:
+                                pass
+                            elif icon == None or menuIcon == None:
+                                print("Some icon not found for {}: minimap: {} // hud: {}".format(veh, icon, menuIcon))
+                            elif consistentMini and icon[len(pre1):] != menuIcon[len(pre2):]:
+                                wrong_menuIcons.append((veh, icon, menuIcon, p))
+
+                            if veh[-len(bf2):] == bf2:
+                                bf2_vehicles.append((veh, icon, menuIcon))
+                            elif veh[-len(prsp):] == prsp:
+                                prsp_vehicles.append((veh, icon, menuIcon))
+                            elif veh[-len(sp):] == sp:
+                                sp_vehicles.append((veh, icon, menuIcon))
+                            else:
+                                base_vehicles[veh] = (veh, icon, menuIcon)
+                            
+                            if name is not None:
+                                names_map[veh] = name
+
+if __name__ == "__main__":
+
+    print()
+    print()
+
+    print("Check if _bf2 vehicles use the same icons as base...")
+    for veh, icon, menuIcon in bf2_vehicles:
+        veh2 = veh[:-len(bf2)]
+        if veh2 in base_vehicles:
+            _, icon2, menuIcon2 = base_vehicles[veh2]
+            if icon != icon2:
+                print("    Different mini_ icons for {} and {}: {} != {}".format(veh2, veh, icon2, icon))
+            if menuIcon != menuIcon2:
+                print("    Different mini_ icons for {} and {}: {} != {}".format(veh2, veh, menuIcon2, menuIcon))
+        else:
+            print("    Base {} not found for {}!".format(veh2, veh))
+    print("Done!")
+
+    print()
+    print()
+
+    print("Check if _prsp vehicles use the same icons as base...")
+    for veh, icon, menuIcon in prsp_vehicles:
+        veh2 = veh[:-len(prsp)]
+        if veh2 in base_vehicles:
+            _, icon2, menuIcon2 = base_vehicles[veh2]
+            if icon != icon2:
+                print("    Different menuIcon_ icons for {} and {}: {} != {}".format(veh2, veh, icon2, icon))
+            if menuIcon != menuIcon2:
+                print("    Different menuIcon_ icons for {} and {}: {} != {}".format(veh2, veh, menuIcon2, menuIcon))
+        else:
+            print("    Base {} not found for {}!".format(veh2, veh))
+    print("Done!")
+
+    print()
+    print()
+
+    print("Check if _sp vehicles use the same icons as base...")
+    for veh, icon, menuIcon in sp_vehicles:
+        veh2 = veh[:-len(sp)]
+        if veh2 in base_vehicles:
+            _, icon2, menuIcon2 = base_vehicles[veh2]
+            if icon != icon2:
+                print("    Different menuIcon_ icons for {} and {}: {} != {}".format(veh2, veh, icon2, icon))
+            if menuIcon != menuIcon2:
+                print("    Different menuIcon_ icons for {} and {}: {} != {}".format(veh2, veh, menuIcon2, menuIcon))
+        else:
+            print("    Base {} not found for {}!".format(veh2, veh))
+    print("Done!")
+
+    print()
+    print()
+
+    print("Unknown mini_ icons (total: {}):".format(len(missing_icons)))
+    for k, v in missing_icons.items():
+        print(k)
+        for v2 in v:
+            print("    {}".format(v2))
+
+    print()
+    print()
+
+    print("Unknown menuIcon_ icons (total: {}):".format(len(missing_menuIcons)))
+    for k, v in missing_menuIcons.items():
+        print(k)
+        for v2 in v:
+            print("    {}".format(v2))
+
+    print()
+    print()
+
+    print("Inconsistent mini_ icons (total: {}):".format(len(inconsistent_icons)))
+    for v in inconsistent_icons:
+        print("    {}".format(v))
+
+    print()
+    print()
+
+    print("Inconsistent menuIcon_ icons (total: {}):".format(len(inconsistent_menuIcons)))
+    for v in inconsistent_menuIcons:
+        print("    {}".format(v))
+
+    print()
+    print()
+
+    print("Vehicles with different mini_ and menuIcon_ icons (total: {}):".format(len(wrong_menuIcons)))
+    for veh, icon, menuIcon, path in wrong_menuIcons:
+        print("    {}: {} != {}".format(veh, icon, menuIcon))
+
+    print()
+    print()
+
+    print("Known Icons + Vehicles:")
+    for k, v in icons_map.items():
+        if len(v) > 0:
+            print("    {}".format(k))
+            for v2 in v:
+                if v2[-len(bf2):] != bf2 and v2[-len(prsp):] != prsp and v2[-len(sp):] != sp:
+                    print("        {}".format(v2))
+
+    print()
+    print()
+
+    print("Unused Icons:")
+    for k, v in icons_map.items():
+        if len(v) == 0:
+            print("    {}".format(k))
+
