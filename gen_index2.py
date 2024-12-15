2b65195715859618b10b4f7595644dee3d953727
CAS_ual_TY <41904979+CAS-ual-TY@users.noreply.github.com>
2024-12-15 22:21:46 +0100

    update

--- gen_index2.py
+++ gen_index2.py
@@ -29,11 +29,6 @@ def makeHead(title):
         "div.inner {",
         "    display: inline-block;",
         "}",
-        "ul {",
-        "    list-style-type: none;",
-        "    padding: 0;",
-        "    margin: 0;",
-        "}",
         "img {",
         "    image-rendering: -moz-crisp-edges;",
         "    image-rendering: -webkit-optimize-contrast;",
@@ -85,11 +80,11 @@ facTr = {
 
 claimingDict = {
   "APC": [["apc_light", "apc_medium", "apc_heavy"], ["ifv_light", "ifv_medium", "ifv_heavy"], ["rec_light", "rec_medium", "rec_medium_scorpion", "rec_heavy"]],
-  "TANK": [["tnk_light", "tnk_medium", "tnk_heavy"]],
+  "Tank": [["tnk_light", "tnk_medium", "tnk_heavy"]],
   "SPG": [["tec_spg"]],
-  "ROCKET": [["tec_rocket"]],
+  "Rocket": [["tec_rocket"]],
   "AAV": [["aav_light", "aav_medium", "aav_heavy"]],
-  "TRANS / TRANSPORT": [["the_scout", "the_scout_escort"], ["the_light", "the_light_escort"], ["the_medium", "the_medium_escort"], ["the_heavy", "the_heavy_chinook"], ["the_heavy_osprey"]],
+  "Trans": [["the_scout", "the_scout_escort"], ["the_light", "the_light_escort"], ["the_medium", "the_medium_escort"], ["the_heavy", "the_heavy_chinook"], ["the_heavy_osprey"]],
   "CAS": [["jet_1_attack", "jet_1_strikefighter", "jet_1_asf"], ["jet_2_attack", "jet_2_strikefighter", "jet_2_asf"], ["jet_4_attack", "jet_4_strikefighter", "jet_4_asf"], ["jet_6_attack", "jet_7_attack", "jet_8_attack"], ["jet_10_attack", "jet_10_strikefighter", "jet_10_asf", "jet_10_as"], ["jet_9_attack", "jet_9_strikefighter", "jet_9_asf"], ["jet_11_attack"]],
 }
 
@@ -130,13 +125,15 @@ if __name__ == "__main__":
     print(file)
 
     lines = []
-    lines.extend(makeHead("Alliance Claiming Index"))
+    lines.extend(makeHead("PR Assets Indexes"))
     lines.extend([
         "<h2>Automatically generated at {} using a script by CAS_ual_TY</h2>".format(time.asctime()),
         "<h2>Vehicles</h2>",
-        "<span><a href=\"squad-to-icon_index.html\">Squad-to-Icon Index (click here)</a></span>",
-        #"<span><a href=\"asset-to-icon_index.html\">PR Assets-to-Icon Index (click here)</a></span>"
-        #"<span><a href=\"faction-asset-to-icon_index.html\">PR Faction-Assets-to-Icon Index (click here)</a></span>",
+        "<span><a href=\"squad-to-icon_index.html\">Asset-to-Icon Index (click here)</a></span>",
+        "<span><a href=\"asset-to-icon_index.html\">PR Assets-to-Icon Index (click here)</a></span>"
+        "<span><a href=\"faction-asset-to-icon_index.html\">PR Faction-Assets-to-Icon Index (click here)</a></span>",
+        "<h2>Kits</h2>",
+        "<span><a href=\"icon-to-kit_index.html\">PR Icon-to-Kit Index (click here)</a></span>",
     ])
     lines.extend(tail)
 
@@ -152,35 +149,36 @@ if __name__ == "__main__":
     print(file)
 
     lines = []
-    lines.extend(makeHead("Alliance Squad-to-Icon Index"))
+    lines.extend(makeHead("PR Icon-to-Assets Index"))
     lines.append("<a href=\"index.html\">Return to Index</a>")
     lines.extend([
         "<h3>Sorted alphabetically by asset name (ignoring faction identifier in front).</h3>",
         "<table>",
-        "<tbody>",
+        "<hr>",
     ])
     
     for squad, list in claimingDict.items():
-        #lines.append("<hr>")
-        #lines.append("<h2>{}</h2>".format(squad))
-        
-        #lines.append("<table>")
-        lines.append("<tr>")
-        
-        lines.append("<td><b>{}</b></td>".format(squad))
-        
-        lines.append("<td><ul>")
+        lines.append("<hr>")
+        lines.append("<h2>{}</h2>".format(squad))
+        lines.append("<table>")
         for sub_list in list:
-            lines.append("<li>")
+            lines.append("<tr>")
             for icon in sub_list:
-                lines.append("<img src=\"./vehicles/{}\">".format("mini_" + icon + ".png"))
-            lines.append("</li>")
-        lines.append("</ul></td>")
-        
-        lines.append("</tr>")
-
-    lines.append("</tbody>")
-    lines.append("</table>")
+                lines.append("<td><img src=\"./vehicles/{}\"></td>".format("mini_" + icon + ".png"))
+            #lines.append("<td>")
+            #lines.append("<b>{}</b>:".format(icon))
+            #lines.append("<ul>")
+            
+            #k = "mini_" + squad + ".tga"
+            #v = icons_map[k]
+            #for v2 in v:
+            #    if v2[-len(bf2):] != bf2 and v2[-len(prsp):] != prsp and v2[-len(sp):] != sp:
+            #        fac, veh = vehToFactionName(v2)
+            #        lines.append("<li>{} <b>{}</b> ({})</li>".format(fac, veh, v2))
+            #lines.append("</ul>")
+            #lines.append("</td>")
+            lines.append("</tr>")
+        lines.append("</table>")
     
     lines.extend(tail)
     with open(file, "w") as f:
